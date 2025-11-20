7a77499748ae5052e72df552d943e2e6306fd308
paulworking2420 <paulworking2420@gmail.com>
2025-11-20 15:24:02 +0700

    Update

--- app/services/database.server.ts
+++ app/services/database.server.ts
@@ -7,11 +7,23 @@ declare global {
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      }
+    }
+  });
   prisma.$connect();
 } else {
   if (!global.__db) {
-    global.__db = new PrismaClient();
+    global.__db = new PrismaClient({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL
+        }
+      }
+    });
     global.__db.$connect();
   }
   prisma = global.__db;
